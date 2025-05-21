bd7544b119f76926e36a4e596056410d7d8e0726
Mkznd <yzundai1@gmail.com>
2025-05-21 19:45:46 +0200

    Revert the background tasks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uuid
 
-from fastapi import FastAPI, HTTPException, BackgroundTasks
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 
 from settings import settings
@@ -13,26 +13,18 @@ app = FastAPI()
 
 
 @app.post("/render")
-def render_video(payload: VideoRequest, background_tasks: BackgroundTasks):
-
+def render_video(payload: VideoRequest):
     tmp_file = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
     output_path = tmp_file.name
     filename = f"{uuid.uuid4().hex}.mp4"
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(settings.bucket_name)
-    blob = bucket.blob(filename)
-    background_tasks.add_task(create_and_upload_video, payload, output_path, blob)
-    return VideoResponse(url=blob.public_url)
-
-
-def create_and_upload_video(payload: VideoRequest, output_path: str, blob):
-    # Create a temporary file to store the video
-
     try:
         create_video(payload, output_path)
-        print("Video created successfully")
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(settings.bucket_name)
+        blob = bucket.blob(filename)
         blob.upload_from_filename(output_path)
-        print("Video uploaded successfully")
         return VideoResponse(url=blob.public_url)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        pass  # You can schedule cleanup later
